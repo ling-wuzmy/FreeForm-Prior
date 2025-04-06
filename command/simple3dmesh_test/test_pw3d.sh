d713b1a090ce9a868dee10bb292e416f1515b8aa
ling-wuzmy <651547502@qq.com>
2025-04-06 21:09:56 +0800

    Update test_pw3d.sh

--- command/simple3dmesh_test/test_pw3d.sh
+++ command/simple3dmesh_test/test_pw3d.sh
@@ -1,1 +1,1 @@
-python main/main.py --cfg ./configs/simple3dmesh_test/baseline_pw3d.yml --experiment_name simple3dmesh_test/baseline_pw3d --gpus 4 --mode test
+CUDA_VISIBLE_DEVICES=0 python main/main.py --cfg ./configs/simple3dmesh_test/baseline_pw3d.yml --experiment_name simple3dmesh_test/baseline_pw3d --gpus 1 --mode test
